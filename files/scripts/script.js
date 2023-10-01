c0eb36820cf24a86ad07a01d0ced10ebe9c841ab
Talut Salako <salakotalut@gmail.com>
2023-10-01 13:31:47 +0100

    update frontend based on database values

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -1,4 +1,16 @@
-document.addEventListener('click', async (event) => {
+document.addEventListener('DOMContentLoaded', ()=>{
+ let tasks = document.querySelectorAll('.task')
+ tasks.forEach(task => {
+  let checkbox = task.querySelector('input[type=checkbox]')
+  let taskTitle = task.querySelector('h2')
+  checkbox.checked
+      ? taskTitle.classList.add('completed')
+      : taskTitle.classList.remove('completed');
+ })
+
+
+
+  document.addEventListener('click', async (event) => {
   if (event.target.classList.contains('delete')) {
     event.preventDefault();
     let taskID = event.target.getAttribute('data-task-id');
@@ -16,11 +28,30 @@ document.addEventListener('click', async (event) => {
   }
 });
 
-document.addEventListener('change', (event) => {
+document.addEventListener('change', async (event) => {
   if (event.target.type === 'checkbox') {
     const isChecked = event.target.checked;
     isChecked
       ? event.target.nextElementSibling.classList.add('completed')
       : event.target.nextElementSibling.classList.remove('completed');
+    let parentElement = event.target.parentNode;
+    let button = parentElement.querySelector('.delete');
+    taskID = button.getAttribute('data-task-id');
+    try {
+      let response = await fetch(
+        `/api/tasks/${taskID}?isCompleted=${isChecked}`,
+        { method: 'PATCH' }
+      );
+      console.log(response)
+      response.status === 200
+        ? console.log('task status updated')
+        : console.log('task status: unable to update');
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
   }
 });
+})
+
+
+
